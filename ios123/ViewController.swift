d73ac481bd12b4df62ae0387035d75692ad42632
20171105120 <476625879@qq.com>
2018-09-27 09:57:16 +0800

    123
    123

---
+++ ios123/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  ios123
+//
+//  Created by 刘洋博 on 2018/9/21.
+//  Copyright © 2018年 刘洋博. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var cin: UITableViewCell!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func number1(_ sender: Any) {
+    }
+    @IBAction func number2(_ sender: Any) {
+    }
+    @IBAction func number3(_ sender: Any) {
+    }
+    @IBAction func add(_ sender: Any) {
+    }
+    @IBAction func result(_ sender: Any) {
+    }
+    
+    
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
